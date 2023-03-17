124276e5025ead7610e5c51c172aff8f6458c3d1
NZhukov <nzhukov.rnd@gmail.com>
2023-03-17 17:22:12 +0400

    final

--- MyDaysApp/ViewController.swift
+++ MyDaysApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var resultButton: UIButton!
+    
+    private var numbersOdDays = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultButton.layer.cornerRadius = 12
     }
 
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        if Date.now > sender.date {
+            let range = sender.date ..< Date.now
+            numbersOdDays = range.formatted(.components(style: .wide, fields: [.day]))
+        }
+    }
+    
+
+    @IBAction func resultButtonTapped() {
+        infoLabel.text = "Ты наслаждаешься жизнью уже \(numbersOdDays)!"
+    }
 
 }
 
